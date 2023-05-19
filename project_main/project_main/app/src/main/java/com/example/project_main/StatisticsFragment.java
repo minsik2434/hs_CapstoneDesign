681bc477ee346686c75d22c1276a9bbb08b1d685
minsik2434 <119111149+minsik2434@users.noreply.github.com>
2023-05-19 17:46:51 +0900

    Add files via upload

---
+++ project_main/project_main/app/src/main/java/com/example/project_main/StatisticsFragment.java
@@ -0,0 +1,80 @@
+package com.example.project_main;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.relex.circleindicator.CircleIndicator3;
+
+
+public class StatisticsFragment extends Fragment {
+
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 3;
+    private CircleIndicator3 mIndicator;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        /**
+         * 가로 슬라이드 뷰 Fragment
+         */
+
+        //ViewPager2
+        mPager = view.findViewById(R.id.viewpager);
+        //Adapter
+        pagerAdapter = new BannerAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //Indicator
+        mIndicator = view.findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page,0);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
+        /**
+         * 이 부분 조정하여 처음 시작하는 이미지 설정.
+         * 2000장 생성하였으니 현재위치 1002로 설정하여
+         * 좌 우로 슬라이딩 할 수 있게 함. 거의 무한대로
+         */
+
+        mPager.setCurrentItem(540); //시작 지점
+        mPager.setOffscreenPageLimit(3); //최대 이미지 수
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position%num_page);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
